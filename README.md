96f814f8a9a71b40a322bedc5d9bf5d558eb4dac
Jack Watters <johnwatt@usc.edu>
2024-03-18 15:48:29 -0700

    Some notes from T3 tutorial.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Team Send
 
 Easily send targeted bulk SMS to groups
+
+## Notes
+
+- To get TRPC out put types
+
+  - `type PostWithUser = RouterOutputs["posts"]["getAll"][number]`
+
+- when loading multiple things ie nav and groups
+
+- [Dayjs](https://day.js.org/docs/en/display/format) for date formatting
---
+++ src/components/ui/SettingActionItem.tsx
@@ -0,0 +1,39 @@
+import { cn } from "@/lib/utils";
+import { Button } from "./button";
+
+interface ISettingActionItemProps {
+  title: string;
+  description: string;
+  actionButtonText: string | React.ReactNode;
+  buttonVariant?: "default" | "destructive";
+  onAction: () => void;
+}
+
+export const SettingActionItem = ({
+  title,
+  description,
+  actionButtonText,
+  buttonVariant = "default",
+  onAction,
+}: ISettingActionItemProps) => {
+  return (
+    <div
+      className={cn(
+        "flex items-center justify-between gap-2 rounded-lg border p-4",
+        buttonVariant === "destructive"
+          ? "border-red-300/80 dark:border-red-900/60"
+          : "dark:border-stone-800",
+      )}
+    >
+      <div className="space-y-0.5">
+        <div className="text-base font-medium">{title}</div>
+        <div className="text-sm text-stone-500 dark:text-stone-400">
+          {description}
+        </div>
+      </div>
+      <Button type="button" variant={buttonVariant} onClick={onAction}>
+        {actionButtonText}
+      </Button>
+    </div>
+  );
+};
