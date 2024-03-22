331952cdcee1e202a6367074a0d47eb8c526ae23
Jack Watters <johnwatt@usc.edu>
2024-03-21 23:24:13 -0700

    Account pages -> static.

--- src/pages/account/profile.tsx
+++ src/pages/account/profile.tsx
@@ -1,17 +1,166 @@
 import { api } from "@/utils/api";
 
 import { AccountLayout } from "@/layouts/AccountLayout";
-import AccountProfileForm from "@/components/account/AccountProfileForm";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { CopyIcon } from "@radix-ui/react-icons";
+import { z } from "zod";
+
+import extractInitials from "@/lib/extractInitials";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Form, FormDescription } from "@/components/ui/form";
+import { FormInput } from "@/components/ui/form-inputs";
+import { toast } from "@/components/ui/use-toast";
+import { Button } from "@/components/ui/button";
+import { genSSRHelpers } from "@/server/helpers/genSSRHelpers";
+
+const userSettingsSchema = z.object({
+  name: z.string().max(40),
+  avatar: z.string().optional(),
+  "avatar-file": z.string().optional(),
+  email: z.string().email(),
+  phone: z.string().optional(),
+  username: z.string(),
+});
+
+type UserSettingsFormSchema = typeof userSettingsSchema;
+
+type UserSettingsFormType = z.infer<typeof userSettingsSchema>;
 
 export default function AccountProfile() {
-  const { data: currentUser } = api.auth.getCurrentUser.useQuery();
+  const { data: user } = api.auth.getCurrentUser.useQuery();
 
-  return currentUser ? (
+  const form = useForm<UserSettingsFormType>({
+    resolver: zodResolver(userSettingsSchema),
+    defaultValues: {
+      name: user?.name ?? "",
+      avatar: user?.avatar ?? "",
+      "avatar-file": "",
+      email: user?.email ?? "",
+      username: user?.username ?? "",
+    },
+  });
+
+  const onSubmit = (data: UserSettingsFormType) => {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  };
+
+  if (!user) {
+    return null;
+  }
+
+  return (
     <AccountLayout
       title="User Profile"
       description={"Manage your account information"}
     >
-      <AccountProfileForm currentUser={currentUser} />
+      <Form {...form}>
+        <div className="flex flex-col pb-4 pt-3">
+          <h2 className="text-xl font-semibold tracking-tight">Edit Account</h2>
+          <FormDescription>Make changes to your profile here.</FormDescription>
+        </div>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-8 py-4 sm:gap-6"
+        >
+          <div className="flex-1 space-y-3">
+            <div className="text-sm font-medium leading-none">User ID</div>
+            <Button
+              className="flex h-10 w-full items-center rounded-md border border-stone-200 bg-white px-3 py-2 text-start text-sm hover:bg-white dark:border-stone-800 dark:bg-stone-900 dark:hover:bg-stone-900"
+              type="button"
+              variant={"ghost"}
+              onClick={() => {
+                toast({
+                  title: "Copied User ID",
+                  description:
+                    "Your user ID has been copied to your clipboard.",
+                });
+                return navigator.clipboard.writeText(user.id);
+              }}
+            >
+              <div className="flex-1">{user?.id}</div>
+              <div className="hover: h-fit rounded-md p-1 hover:bg-stone-100 dark:hover:bg-stone-800">
+                <CopyIcon className="h-4 w-4 text-stone-500 dark:text-stone-400" />
+              </div>
+            </Button>
+          </div>
+          <div className="flex justify-between gap-12">
+            <div className="flex-1">
+              <FormInput<UserSettingsFormSchema>
+                name="avatar-file"
+                label="Group Avatar"
+                type="file"
+                accept=".png, .jpg, .jpeg"
+                className="dark:file:text-white "
+                control={form.control}
+              />
+            </div>
+            {(form.watch("name") ??
+              form.watch("avatar") ??
+              form.watch("avatar-file")) && (
+              <Avatar className="h-20 w-20">
+                <AvatarImage
+                  src={form.watch("avatar-file") ?? form.watch("avatar")}
+                  alt="Group Avatar"
+                />
+                <AvatarFallback className="text-4xl font-medium ">
+                  {extractInitials(form.watch("name"))}
+                </AvatarFallback>
+              </Avatar>
+            )}
+          </div>
+          <FormInput<UserSettingsFormSchema>
+            control={form.control}
+            name="name"
+            label="Name"
+            placeholder="Add your full Name"
+          />
+          <FormInput<UserSettingsFormSchema>
+            control={form.control}
+            name="username"
+            label="Username"
+            placeholder="Add a Username"
+          />
+          <FormInput<UserSettingsFormSchema>
+            control={form.control}
+            name="email"
+            label="Email"
+            placeholder="Add your email"
+          />
+          <FormInput<UserSettingsFormSchema>
+            control={form.control}
+            name="phone"
+            label="Phone"
+            placeholder="Add your phone number"
+          />
+          <div className="py-4">
+            <Button type="submit" className="w-full">
+              Save
+            </Button>
+          </div>
+        </form>
+      </Form>
     </AccountLayout>
-  ) : null;
+  );
 }
+
+export const getStaticProps = async () => {
+  const helpers = genSSRHelpers();
+
+  await helpers.auth.getCurrentUser.prefetch();
+
+  return {
+    props: {
+      trpcState: helpers.dehydrate(),
+    },
+  };
+};
--- src/pages/account/settings.tsx
+++ src/pages/account/settings.tsx
@@ -1,20 +1,27 @@
 import { AccountLayout } from "@/layouts/AccountLayout";
-import AccountConnections from "@/components/account/AccountConnections";
-import AccountDangerZone from "@/components/account/AccountDangerActions";
+import { SettingActionItem } from "@/components/ui/setting-action-item";
 import { api } from "@/utils/api";
 
+import { genSSRHelpers } from "@/server/helpers/genSSRHelpers";
 export default function AccountSettings() {
-  const { data: currentUser } = api.auth.getCurrentUser.useQuery();
+  const { data: user } = api.auth.getCurrentUser.useQuery();
 
-  return currentUser ? (
+  const handleExport = () => {
+    console.log("Export data");
+  };
+
+  if (!user) {
+    return null;
+  }
+
+  return (
     <AccountLayout
       title="User Settings"
       description={"View and edit your settings."}
     >
       <div
         className="mb-2 flex flex-col border-b pb-2 pt-3
-        dark:border-stone-500 dark:border-opacity-20
-      "
+        dark:border-stone-500 dark:border-opacity-20"
       >
         <h2 className="text-xl font-semibold tracking-tight">
           Edit Account Settings
@@ -23,12 +30,98 @@ export default function AccountSettings() {
           Make changes to account settings here.
         </div>
       </div>
-      <div className="pt-4">
-        <AccountConnections currentUser={currentUser} />
-      </div>
-      <div className="pt-4">
-        <AccountDangerZone />
-      </div>
+      <section>
+        <div className="flex flex-col pb-4 pt-7">
+          <h2 className="text-lg font-semibold tracking-tight">Connections</h2>
+          <p className="text-xs text-stone-500 dark:text-stone-400">
+            Manage your communication methods.
+          </p>
+        </div>
+        <div className="flex flex-col gap-8 py-4 sm:gap-6">
+          <SettingActionItem
+            title="Email"
+            description={
+              user.nodeMailer
+                ? `Connected via Nodemailer: ${user.nodeMailer}.`
+                : "Send emails via Nodemailer to members of this group."
+            }
+            actionButtonText={user.nodeMailer ? "Disconnect" : "Connect"}
+            buttonVariant={user.nodeMailer ? "destructive" : "default"}
+            onAction={() => {
+              console.log("Connect email");
+            }}
+          />
+          <SettingActionItem
+            title="Phone"
+            description={
+              user.twilio
+                ? `Connected via Twilio: ${user.twilio}.`
+                : "Send texts via Twilio to members of this group."
+            }
+            actionButtonText={user.twilio ? "Disconnect" : "Connect"}
+            buttonVariant={user.twilio ? "destructive" : "default"}
+            onAction={() => {
+              console.log("Connect phone");
+            }}
+          />
+        </div>
+      </section>
+      <section>
+        <div className="flex flex-col pb-4 pt-7">
+          <h2 className="text-lg font-semibold tracking-tight">
+            Account Actions
+          </h2>
+          <p className="text-xs text-stone-500 dark:text-stone-400">
+            Manage your account settings.
+          </p>
+        </div>
+        <div className="flex flex-col gap-8 py-4 sm:gap-6">
+          <SettingActionItem
+            title="Export Your Data"
+            description="Download a file containing your account information for safekeeping."
+            actionButtonText="Export Data"
+            onAction={handleExport}
+          />
+          <SettingActionItem
+            title="Reset Password"
+            description="Send a password reset link to your email address."
+            actionButtonText="Reset Password"
+            onAction={() => {
+              console.log("Reset password");
+            }}
+          />
+          <SettingActionItem
+            title="Archive your account"
+            description="Once you archive your account, you can restore it later."
+            actionButtonText="Archive Account"
+            buttonVariant="destructive"
+            onAction={() => {
+              console.log("Archive group");
+            }}
+          />
+          <SettingActionItem
+            title="Delete your account"
+            description="Once you delete your account, there is no going back. Please be certain."
+            actionButtonText="Delete Account"
+            buttonVariant="destructive"
+            onAction={() => {
+              console.log("Archive group");
+            }}
+          />
+        </div>
+      </section>
     </AccountLayout>
-  ) : null;
+  );
 }
+
+export const getStaticProps = async () => {
+  const helpers = genSSRHelpers();
+
+  await helpers.auth.getCurrentUser.prefetch();
+
+  return {
+    props: {
+      trpcState: helpers.dehydrate(),
+    },
+  };
+};
