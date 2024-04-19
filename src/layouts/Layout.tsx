5023e5d506dcfe39283bf9843ba3311901eb9c15
Jack Watters <johnwatt@usc.edu>
2024-04-19 12:05:20 -0700

    Moved Pusher logic to _app to fix problem where new connection created every rerender.

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,18 +1,5 @@
 import Nav from "@/components/nav/Nav";
-import { Toaster } from "@/components/ui/toaster";
 import Head from "next/head";
-import { useEffect } from "react";
-import { useSession } from "next-auth/react";
-import Pusher from "pusher-js";
-
-import { env } from "@/env";
-import { toast } from "@/components/ui/use-toast";
-
-export interface MessageSendData {
-  status: "sent" | "failed";
-  messageId: string;
-  groupName: string;
-}
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -25,39 +12,6 @@ export default function Layout({
   title = "Team Send",
   description = "Easily send targeted bulk SMS to groups",
 }: LayoutProps) {
-  const session = useSession();
-
-  const userId = session?.data?.user?.id;
-  useEffect(() => {
-    const pusher = new Pusher(env.NEXT_PUBLIC_PUSHER_KEY, {
-      cluster: env.NEXT_PUBLIC_PUSHER_CLUSTER,
-    });
-
-    const channel = pusher.subscribe(`user-${userId}`);
-    channel.bind(
-      "message-status",
-      ({ status, messageId, groupName }: MessageSendData) => {
-        if (status === "sent") {
-          toast({
-            title: `Message ${messageId} sent`,
-            description: `Message to group ${groupName} has been sent successfully`,
-          });
-        } else {
-          toast({
-            title: `Message ${messageId} failed to send`,
-            description: `Message to group ${groupName} failed to send`,
-            variant: "destructive",
-          });
-        }
-      },
-    );
-
-    return () => {
-      channel.unbind_all();
-      channel.unsubscribe();
-    };
-  }, [userId]);
-
   return (
     <>
       <Head>
@@ -71,7 +25,6 @@ export default function Layout({
           <div className="xs:px-12 px-6 py-6 sm:px-24">{children}</div>
         </main>
       </div>
-      <Toaster />
     </>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, useSession } from "next-auth/react";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { type Session } from "next-auth";
 import { type AppType } from "next/app";
+import { useEffect, useRef } from "react";
+import Pusher, { type Channel } from "pusher-js";
 
+import { env } from "@/env";
+import { toast } from "@/components/ui/use-toast";
+import { Toaster } from "@/components/ui/toaster";
 import { api } from "@/utils/api";
-
 import "@/styles/globals.css";
 
 const inter = Inter({
@@ -13,18 +17,70 @@ const inter = Inter({
   variable: "--font-sans",
 });
 
+export interface MessageStatusPush {
+  status: "sent" | "failed";
+  messageId: string;
+  groupName: string;
+}
+
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
     <SessionProvider session={session}>
-      <div className={`font-sans ${inter.variable}`}>
-        <Component {...pageProps} />
-      </div>
-      <SpeedInsights />
+      <PusherProvider>
+        <div className={`font-sans ${inter.variable}`}>
+          <Component {...pageProps} />
+          <Toaster />
+        </div>
+        <SpeedInsights />
+      </PusherProvider>
     </SessionProvider>
   );
 };
 
 export default api.withTRPC(MyApp);
+
+export function PusherProvider({ children }: { children: React.ReactNode }) {
+  const { data: session } = useSession();
+
+  const userId = session?.user?.id;
+  useEffect(() => {
+    if (!userId) return;
+    const pusher = new Pusher(env.NEXT_PUBLIC_PUSHER_KEY, {
+      cluster: env.NEXT_PUBLIC_PUSHER_CLUSTER,
+    });
+    const channel = pusher.subscribe(`user-${userId}`);
+    channel.bind(
+      "message-status",
+      ({ status, messageId, groupName }: MessageStatusPush) => {
+        console.log("status");
+        if (status === "sent") {
+          toast({
+            title: `Message ${messageId} sent`,
+            description: `Message to group ${groupName} has been sent successfully`,
+          });
+        } else {
+          toast({
+            title: `Message ${messageId} failed to send`,
+            description: `Message to group ${groupName} failed to send`,
+            variant: "destructive",
+          });
+        }
+      },
+    );
+
+    return () => {
+      if (pusher) {
+        pusher.disconnect();
+      }
+      if (channel) {
+        channel.unbind_all();
+        channel.unsubscribe();
+      }
+    };
+  }, [userId]);
+
+  return <>{children}</>;
+}
