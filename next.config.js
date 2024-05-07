1f386fc00af34b3d12d8850e92a6b0acd60e92cf
Jack Watters <johnwatt@usc.edu>
2024-05-06 20:42:32 -0700

    Eslint/prettier/husky -> biome + lefthook.

--- next.config.js
+++ next.config.js
@@ -6,34 +6,40 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const coreConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
 
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	/**
+	 * If you are using `appDir` then you must comment the below `i18n` config out.
+	 *
+	 * @see https://github.com/vercel/next.js/issues/41980
+	 */
+	i18n: {
+		locales: ["en"],
+		defaultLocale: "en",
+	},
 
-  images: {
-    domains: ["res.cloudinary.com"],
-  },
+	images: {
+		domains: ["res.cloudinary.com"],
+	},
 
-  async rewrites() {
-    return [
-      {
-        source: "/ingest/static/:path*",
-        destination: "https://us-assets.i.posthog.com/static/:path*",
-      },
-      {
-        source: "/ingest/:path*",
-        destination: "https://us.i.posthog.com/:path*",
-      },
-    ];
-  },
+	async rewrites() {
+		return [
+			{
+				source: "/ingest/static/:path*",
+				destination: "https://us-assets.i.posthog.com/static/:path*",
+			},
+			{
+				source: "/ingest/:path*",
+				destination: "https://us.i.posthog.com/:path*",
+			},
+		];
+	},
 };
 
 // Injected content via Sentry wizard below
@@ -41,44 +47,44 @@ const coreConfig = {
 import { withSentryConfig } from "@sentry/nextjs";
 
 const config = withSentryConfig(
-  coreConfig,
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
+	coreConfig,
+	{
+		// For all available options, see:
+		// https://github.com/getsentry/sentry-webpack-plugin#options
 
-    // Suppresses source map uploading logs during build
-    silent: true,
-    org: "yats-co",
-    project: "teamsend",
-  },
-  {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+		// Suppresses source map uploading logs during build
+		silent: true,
+		org: "yats-co",
+		project: "teamsend",
+	},
+	{
+		// For all available options, see:
+		// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
+		// Upload a larger set of source maps for prettier stack traces (increases build time)
+		widenClientFileUpload: true,
 
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
+		// Transpiles SDK to be compatible with IE11 (increases bundle size)
+		transpileClientSDK: true,
 
-    // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-    // This can increase your server load as well as your hosting bill.
-    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-    // side errors will fail.
-    tunnelRoute: "/monitoring",
+		// Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+		// This can increase your server load as well as your hosting bill.
+		// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+		// side errors will fail.
+		tunnelRoute: "/monitoring",
 
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
+		// Hides source maps from generated client bundles
+		hideSourceMaps: true,
 
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
+		// Automatically tree-shake Sentry logger statements to reduce bundle size
+		disableLogger: true,
 
-    // Enables automatic instrumentation of Vercel Cron Monitors.
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
-  },
+		// Enables automatic instrumentation of Vercel Cron Monitors.
+		// See the following for more information:
+		// https://docs.sentry.io/product/crons/
+		// https://vercel.com/docs/cron-jobs
+		automaticVercelMonitors: true,
+	},
 );
 
 export default config;
