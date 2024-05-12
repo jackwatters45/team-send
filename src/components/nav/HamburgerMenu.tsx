60974a817169108cf8d7ca3a71092c7d51bcf9c3
Jack Watters <johnwatt@usc.edu>
2024-05-11 22:35:06 -0700

    Add dashboard to nav.

--- src/components/nav/HamburgerMenu.tsx
+++ src/components/nav/HamburgerMenu.tsx
@@ -2,10 +2,10 @@ import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { signOut, useSession } from "next-auth/react";
 
 import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
 } from "@/components/ui/accordion";
 import { Button } from "../ui/button";
 import { Separator } from "../ui/separator";
@@ -13,91 +13,98 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Link from "next/link";
 
 export default function HamburgerMenu() {
-  const userId = useSession().data?.user?.id;
+	const userId = useSession().data?.user?.id;
 
-  const handleLogout = async () => await signOut();
+	const handleLogout = async () => await signOut();
 
-  return (
-    <Sheet>
-      <SheetTrigger asChild>
-        <Button variant="ghost" className="h-fit p-2">
-          <HamburgerMenuIcon className="h-5 w-5" />
-        </Button>
-      </SheetTrigger>
-      <SheetContent>
-        <div className="grid h-full py-4">
-          <Accordion type="multiple">
-            <Link
-              href="/"
-              className="flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline"
-            >
-              Home
-            </Link>
-            <Separator className="dark:bg-stone-500/20" />
-            <Link
-              className="flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline"
-              href="https://github.com/jackwatters45/team-send"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Github
-            </Link>
-            <Separator className="dark:bg-stone-500/20" />
-            <Link
+	return (
+		<Sheet>
+			<SheetTrigger asChild>
+				<Button variant="ghost" className="h-fit p-2">
+					<HamburgerMenuIcon className="h-5 w-5" />
+				</Button>
+			</SheetTrigger>
+			<SheetContent>
+				<div className="grid h-full py-4">
+					<Accordion type="multiple">
+						<Link
+							href="/"
+							className="flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline"
+						>
+							Home
+						</Link>
+						<Separator className="dark:bg-stone-500/20" />
+						<Link
+							href={"/dashboard"}
+							className="flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline"
+						>
+							Dashboard
+						</Link>
+						<Separator className="dark:bg-stone-500/20" />
+						<Link
+							className="flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline"
+							href="https://github.com/jackwatters45/team-send"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Github
+						</Link>
+						<Separator className="dark:bg-stone-500/20" />
+						{/* <Link
               href={"/docs"}
               className="flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline"
             >
               Docs
-            </Link>
-            <Separator className="dark:bg-stone-500/20" />
-            <Link
-              href={"/group/create"}
-              className="flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline"
-            >
-              Create Group
-            </Link>
-            <Separator className="dark:bg-stone-500/20" />
-            {userId ? (
-              <AccordionItem value="item-1">
-                <AccordionTrigger>Account</AccordionTrigger>
-                <AccordionContent className="space-y-3 px-4">
-                  <Link
-                    href="/account/profile"
-                    className="flex h-10 items-center rounded-md px-2 font-medium hover:bg-stone-100 dark:hover:bg-stone-800"
-                  >
-                    Profile
-                  </Link>
-                  <Separator className="dark:bg-stone-500/20" />
-                  <Link
-                    href="/account/settings"
-                    className="flex h-10 items-center rounded-md px-2 font-medium hover:bg-stone-100 dark:hover:bg-stone-800"
-                  >
-                    Settings
-                  </Link>
-                  <Separator className="dark:bg-stone-500/20" />
-                  <Button
-                    onClick={handleLogout}
-                    variant={"ghost"}
-                    className="w-full justify-start px-2 py-0"
-                  >
-                    Log out
-                  </Button>
-                </AccordionContent>
-              </AccordionItem>
-            ) : (
-              <Link
-                href="/login"
-                className="flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline"
-              >
-                Login
-              </Link>
-            )}
-          </Accordion>
-          {/* <div className="self-end">
+            </Link> */}
+						{/* <Separator className="dark:bg-stone-500/20" /> */}
+						<Link
+							href={"/group/create"}
+							className="flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline"
+						>
+							Create Group
+						</Link>
+						<Separator className="dark:bg-stone-500/20" />
+						{userId ? (
+							<AccordionItem value="item-1">
+								<AccordionTrigger>Account</AccordionTrigger>
+								<AccordionContent className="space-y-3 px-4">
+									<Link
+										href="/account/profile"
+										className="flex h-10 items-center rounded-md px-2 font-medium hover:bg-stone-100 dark:hover:bg-stone-800"
+									>
+										Profile
+									</Link>
+									<Separator className="dark:bg-stone-500/20" />
+									<Link
+										href="/account/settings"
+										className="flex h-10 items-center rounded-md px-2 font-medium hover:bg-stone-100 dark:hover:bg-stone-800"
+									>
+										Settings
+									</Link>
+									<Separator className="dark:bg-stone-500/20" />
+									<Button
+										onClick={handleLogout}
+										variant={"ghost"}
+										className="w-full justify-start px-2 py-0"
+									>
+										Log out
+									</Button>
+								</AccordionContent>
+							</AccordionItem>
+						) : (
+							<Link
+								href="/login"
+								className="flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline"
+							>
+								Login
+							</Link>
+						)}
+					</Accordion>
+					{/* <div className="self-end">
 						<SearchButton className="w-full" />
 					</div> */}
-        </div>
-      </SheetContent>
-    </Sheet>
-  );
+				</div>
+			</SheetContent>
+		</Sheet>
+	);
 }
--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -2,77 +2,81 @@ import Image from "next/image";
 
 import UserNav from "./UserNav";
 import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
+	NavigationMenu,
+	NavigationMenuItem,
+	NavigationMenuLink,
 } from "../ui/navigation-menu";
 import { useSession } from "next-auth/react";
 import { ButtonLink } from "../ui/button";
 import HamburgerMenu from "./HamburgerMenu";
 
 export default function Nav() {
-  const session = useSession();
+	const session = useSession();
 
-  return (
-    <NavigationMenu className="max-w-screen fixed z-50 flex  h-14 w-screen justify-center border-b bg-stone-50 bg-opacity-90 px-8 dark:border-stone-500 dark:border-opacity-20 dark:bg-stone-950 dark:bg-opacity-80">
-      <div className="flex w-full max-w-screen-xl items-center justify-between">
-        <NavigationMenuItem>
-          <NavigationMenuLink href="/" className="flex items-center gap-3">
-            <Image
-              src="/favicon.ico"
-              alt="Company Logo"
-              width={20}
-              height={20}
-            />
-            <span>Team Send</span>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-        <div className="hidden items-center gap-4 md:flex">
-          <NavigationMenuItem className="pr-2">
-            <NavigationMenuLink
-              className="text-sm text-stone-400 hover:text-stone-500 dark:text-stone-400 hover:dark:text-stone-300"
-              href="https://github.com/jackwatters45/team-send"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Github
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem className="pr-2">
-            <NavigationMenuLink
-              className="text-sm text-stone-400 hover:text-stone-500 dark:text-stone-400 hover:dark:text-stone-300"
-              href={"/docs"}
-              type="button"
-            >
-              Docs
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem className="pr-2">
-            <NavigationMenuLink
-              className="text-sm text-stone-400 hover:text-stone-500 dark:text-stone-400 hover:dark:text-stone-300"
-              href={"/group/create"}
-              type="button"
-            >
-              Create Group
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          {/* <NavigationMenuItem>
+	return (
+		<NavigationMenu className="max-w-screen fixed z-50 flex  h-14 w-screen justify-center border-b bg-stone-50 bg-opacity-90 px-8 dark:border-stone-500 dark:border-opacity-20 dark:bg-stone-950 dark:bg-opacity-80">
+			<div className="flex w-full max-w-screen-xl items-center justify-between">
+				<NavigationMenuItem>
+					<NavigationMenuLink href="/" className="flex items-center gap-3">
+						<Image src="/favicon.ico" alt="Company Logo" width={20} height={20} />
+						<span>Team Send</span>
+					</NavigationMenuLink>
+				</NavigationMenuItem>
+				<div className="hidden items-center gap-4 md:flex">
+					<NavigationMenuItem className="pr-2">
+						<NavigationMenuLink
+							className="text-sm text-stone-400 hover:text-stone-500 dark:text-stone-400 hover:dark:text-stone-300"
+							href={"/dashboard"}
+							type="button"
+						>
+							Dashboard
+						</NavigationMenuLink>
+					</NavigationMenuItem>
+					<NavigationMenuItem className="pr-2">
+						<NavigationMenuLink
+							className="text-sm text-stone-400 hover:text-stone-500 dark:text-stone-400 hover:dark:text-stone-300"
+							href="https://github.com/jackwatters45/team-send"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Github
+						</NavigationMenuLink>
+					</NavigationMenuItem>
+					<NavigationMenuItem className="pr-2">
+						<NavigationMenuLink
+							className="text-sm text-stone-400 hover:text-stone-500 dark:text-stone-400 hover:dark:text-stone-300"
+							href={"/docs"}
+							type="button"
+						>
+							Docs
+						</NavigationMenuLink>
+					</NavigationMenuItem>
+					<NavigationMenuItem className="pr-2">
+						<NavigationMenuLink
+							className="text-sm text-stone-400 hover:text-stone-500 dark:text-stone-400 hover:dark:text-stone-300"
+							href={"/group/create"}
+							type="button"
+						>
+							Create Group
+						</NavigationMenuLink>
+					</NavigationMenuItem>
+					{/* <NavigationMenuItem>
 						<SearchButton />
 					</NavigationMenuItem> */}
-          {session.status !== "unauthenticated" ? (
-            <NavigationMenuItem style={{ lineHeight: "0" }}>
-              <UserNav />
-            </NavigationMenuItem>
-          ) : (
-            <ButtonLink href="/login" className="h-8 px-3 py-0">
-              Login
-            </ButtonLink>
-          )}
-        </div>
-        <div className="block md:hidden">
-          <HamburgerMenu />
-        </div>
-      </div>
-    </NavigationMenu>
-  );
+					{session.status !== "unauthenticated" ? (
+						<NavigationMenuItem style={{ lineHeight: "0" }}>
+							<UserNav />
+						</NavigationMenuItem>
+					) : (
+						<ButtonLink href="/login" className="h-8 px-3 py-0">
+							Login
+						</ButtonLink>
+					)}
+				</div>
+				<div className="block md:hidden">
+					<HamburgerMenu />
+				</div>
+			</div>
+		</NavigationMenu>
+	);
 }
